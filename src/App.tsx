a013d6779681d4b00cadeceba50b696fb0f7bb83
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2025-01-12 11:01:48 +0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-export default function App (){
-  return(
+import { Route, Routes } from "react-router-dom";
+import { HomePage } from "./pages";
+
+export default function App() {
+  return (
     <>
-    
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+       
+      </Routes>
     </>
   )
 }
